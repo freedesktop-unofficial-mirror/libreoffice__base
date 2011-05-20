e36e3da26ebb6e80f3b2a6b35a65f9ae47da0ba5
Joseph Powers <jpowers27@cox.net>
2011-05-20 05:56:35 -0700

    Remove an un-needed #include <svtools/treelist.hxx>

--- dbaccess/source/ui/browser/dsbrowserDnD.cxx
+++ dbaccess/source/ui/browser/dsbrowserDnD.cxx
@@ -2,7 +2,7 @@
 /*************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- * 
+ *
  * Copyright 2000, 2010 Oracle and/or its affiliates.
  *
  * OpenOffice.org - a multi-platform office productivity suite
@@ -48,7 +48,6 @@
 #include <connectivity/dbexception.hxx>
 #include <connectivity/dbtools.hxx>
 #include <cppuhelper/exc_hlp.hxx>
-#include <svtools/treelist.hxx>
 #include <svx/dataaccessdescriptor.hxx>
 #include <tools/diagnose_ex.h>
 #include <osl/diagnose.h>
